5d27a9621c3f2578c0c0e654263aa2667b494631
Matt Ruffner <mattruffner7@gmail.com>
2019-02-14 20:16:10 -0500

    added plot titles and axis labels. added published matlab pdf with high res figs.

--- projects/p1/p1.m
+++ projects/p1/p1.m
@@ -101,19 +101,19 @@ figure(3)
 subplot(3,1,1)
 Ph1=Xk(:,Sidx);
 plot(Ph1)
-title('S Phoneme (frame 5)')
+title('S Phoneme (frame 4)')
 xlabel('Sample number (relative to frame)')
 
 subplot(3,1,2)
 Ph2=Xk(:,Uidx);
 plot(Ph2)
-title('U Phoneme (frame 14)');
+title('U Phoneme (frame 8)');
 xlabel('Sample number (relative to frame)')
 
 subplot(3,1,3)
 Ph3=Xk(:,Nidx);
 plot(Ph3)
-title('N Phoneme (frame 20)');
+title('N Phoneme (frame 12)');
 xlabel('Sample number (relative to frame)')
 
 
@@ -123,6 +123,7 @@ xlabel('Sample number (relative to frame)')
 % APPLY A HAMMING WINDOW TO EACH PHONEME AND COMPUTE THE LOG MAGNITUDE
 W=hamming(Fr);
 Nfft=Fr;
+%Fax=Fs/2*[0:1/Nfft:1-1/Nfft];
 Fax=Fs*[0:Nfft-1]/Nfft;
 
 % COMPUTE PHONEME LOG MAGNITUDE
@@ -133,11 +134,20 @@ Ph3mag=20*log10(abs(fft(Ph3.*W,Nfft)));
 % PLOT LOG MAGNITUDES OF PHONEMES
 figure(4)
 subplot(3,1,1)
-plot(Fax,Ph1mag)
+plot(Fax(1:length(Fax)/2),Ph1mag(1:length(Ph1mag)/2))
+title('Log Magnitude Spectrum of "s" Phoneme')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude (dB)')
 subplot(3,1,2)
-plot(Fax,Ph2mag)
+plot(Fax(1:length(Fax)/2),Ph2mag(1:length(Ph2mag)/2))
+title('Log Magnitude Spectrum of "u" Phoneme')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude (dB)')
 subplot(3,1,3)
-plot(Fax,Ph3mag)
+plot(Fax(1:length(Fax)/2),Ph3mag(1:length(Ph3mag)/2))
+title('Log Magnitude Spectrum of "n" Phoneme')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude (dB)')
 
 
 %% 5. CEPSTRUM
@@ -150,14 +160,20 @@ Ph3cep=ifft(log(abs(fft(W.*Ph3,Fr))));
 
 figure(5)
 subplot(3,1,1)
-plot(Ph1cep)
+plot(Ph1cep(1:length(Ph1cep)/2))
+title('Real Cepstrum of the "s" Phoneme')
+xlabel('Lags (in samples)')
+ylabel('Cepstral Amplitude')
 subplot(3,1,2)
-plot(Ph2cep)
+plot(Ph2cep(1:length(Ph2cep)/2))
+title('Real Cepstrum of the "u" Phoneme')
+xlabel('Lags (in samples)')
+ylabel('Cepstral Amplitude')
 subplot(3,1,3)
-plot(Ph3cep)
-
-% peak of u phoneme at idx=4 ->  156.25Hz, idx=83 -> 3242.1875Hz
-% peak of n phoneme at idx=69 -> 2695.3125Hz
+plot(Ph3cep(1:length(Ph3cep)/2))
+title('Real Cepstrum of the "n" Phoneme')
+xlabel('Lags (in samples)')
+ylabel('Cepstral Amplitude')
 
 
 %% 6. LIFTERED SPECTRUM
@@ -184,11 +200,20 @@ Ph3Lft=fft(Ph3cep);
 % PLOT THE LIFTERED SPECTRA
 figure(6)
 subplot(3,1,1)
-plot(Fax,real(Ph1Lft))
+plot(Fax(1:length(Fax)/2),real(Ph1Lft(1:length(Ph1Lft)/2)))
+title('Liftered Log Magnitude Spectral Envelope of the "s" Phoneme')
+xlabel('Frequency (Hz)')
+ylabel('Log Spectral Magnitude')
 subplot(3,1,2)
-plot(Fax,real(Ph2Lft))
+plot(Fax(1:length(Fax)/2),real(Ph2Lft(1:length(Ph2Lft)/2)))
+title('Liftered Log Magnitude Spectral Envelope of the "u" Phoneme')
+xlabel('Frequency (Hz)')
+ylabel('Log Spectral Magnitude')
 subplot(3,1,3)
-plot(Fax,real(Ph3Lft))
+plot(Fax(1:length(Fax)/2),real(Ph3Lft(1:length(Ph3Lft)/2)))
+title('Liftered Log Magnitude Spectral Envelope of the "n" Phoneme')
+xlabel('Frequency (Hz)')
+ylabel('Log Spectral Magnitude')
 
 
 
@@ -209,34 +234,59 @@ Ph3LPC14=lpc(W.*Ph3,14);
 Ph3LPC40=lpc(W.*Ph3,40);
 
 figure(7)
+
 subplot(3,3,1)
-plot(Fax,20*log10(abs(freqz(1,Ph1LPC4))))
+lpcr=20*log10(abs(freqz(1,Ph1LPC4)));
+lpcr=lpcr(1:length(lpcr)/2);
+plot(Fax(1:length(Fax)/2),lpcr)
 title('LPC of degree 4 for "s" phoneme')
+
 subplot(3,3,2)
-plot(Fax,20*log10(abs(freqz(1,Ph1LPC14))))
+lpcr=20*log10(abs(freqz(1,Ph1LPC14)));
+lpcr=lpcr(1:length(lpcr)/2);
+plot(Fax(1:length(Fax)/2),lpcr)
 title('LPC of degree 14 for "s" phoneme')
+
 subplot(3,3,3)
-plot(Fax,20*log10(abs(freqz(1,Ph1LPC40))))
+lpcr=20*log10(abs(freqz(1,Ph1LPC40)));
+lpcr=lpcr(1:length(lpcr)/2);
+plot(Fax(1:length(Fax)/2),lpcr)
 title('LPC of degree 40 for "s" phoneme')
 
 subplot(3,3,4)
-plot(Fax,20*log10(abs(freqz(1,Ph2LPC4))))
+lpcr=20*log10(abs(freqz(1,Ph2LPC4)));
+lpcr=lpcr(1:length(lpcr)/2);
+plot(Fax(1:length(Fax)/2),lpcr)
 title('LPC of degree 4 for "u" phoneme')
+
 subplot(3,3,5)
-plot(Fax,20*log10(abs(freqz(1,Ph2LPC14))))
+lpcr=20*log10(abs(freqz(1,Ph2LPC14)));
+lpcr=lpcr(1:length(lpcr)/2);
+plot(Fax(1:length(Fax)/2),lpcr)
 title('LPC of degree 14 for "u" phoneme')
+
 subplot(3,3,6)
-plot(Fax,20*log10(abs(freqz(1,Ph2LPC40))))
+lpcr=20*log10(abs(freqz(1,Ph2LPC40)));
+lpcr=lpcr(1:length(lpcr)/2);
+plot(Fax(1:length(Fax)/2),lpcr)
 title('LPC of degree 40 for "u" phoneme')
 
 subplot(3,3,7)
-plot(Fax,20*log10(abs(freqz(1,Ph3LPC4))))
+lpcr=20*log10(abs(freqz(1,Ph3LPC4)));
+lpcr=lpcr(1:length(lpcr)/2);
+plot(Fax(1:length(Fax)/2),lpcr)
 title('LPC of degree 4 for "n" phoneme')
+
 subplot(3,3,8)
-plot(Fax,20*log10(abs(freqz(1,Ph3LPC14))))
+lpcr=20*log10(abs(freqz(1,Ph3LPC14)));
+lpcr=lpcr(1:length(lpcr)/2);
+plot(Fax(1:length(Fax)/2),lpcr)
 title('LPC of degree 14 for "n" phoneme')
+
 subplot(3,3,9)
-plot(Fax,20*log10(abs(freqz(1,Ph3LPC40))))
+lpcr=20*log10(abs(freqz(1,Ph3LPC40)));
+lpcr=lpcr(1:length(lpcr)/2);
+plot(Fax(1:length(Fax)/2),lpcr)
 title('LPC of degree 40 for "n" phoneme')
 
 
@@ -250,17 +300,20 @@ Nfrms=length(X)/Fr;
 Xres=[];
 for i=1:Nfrms
     res=lpc(Xk(:,i),14);
-    temp=filter(res,1,Xk(:,i));
-    Xres(i,:)=temp;
+    if i==1
+        temp=filter(res,1,Xk(:,i));
+        Xres(i,:)=temp;
+    else
+        temp=filter(res,1,[Xk(:,i-1);Xk(:,i)]);
+        Xres(i,:)=temp(Fr+1:end);
+    end
 end
 
 figure(8);
 plot(reshape(Xres',Fr*Nfrms,1))
 title('LPC Residual Signal')
 xlabel('Time (in samples)')
-
-
-
+ylabel('Residual Amplitude')
 
 Ph1r=lpc(Ph1,14);
 Ph2r=lpc(Ph2,14);
@@ -283,8 +336,4 @@ subplot(3,1,3)
 plot(Ph3rf)
 title('LPC Residual for "n" Phoneme')
 
-
-%% 10. FORMANT ANALYSIS
-%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%
 
