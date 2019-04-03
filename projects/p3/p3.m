408bdf310f60eb50e5a6e1e4fa69209be0d13066
Matt Ruffner <mattruffner7@gmail.com>
2019-04-02 23:03:24 -0400

    started project 3

---
+++ projects/p3/p3.m
@@ -0,0 +1,9 @@
+%% Project 3 main script
+% EE699 Speech Processing
+% Spring 2019
+
+[sig,fs] = audioread('ahhh.wav');
+
+s = tpss(sig,fs,1,[1 1]);
+
+soundsc(s,fs)
---
+++ projects/p3/tpss.m
@@ -0,0 +1,19 @@
+function S=tpss(s,fs,ts,ps)
+% function to perform simulataneous time scaling and pitch shifting
+% s:  input signal
+% fs: sample rate
+% ts: scalar time scaling factor
+% ps: 2 element vector of pitch shifting curve
+%     [initial_pitch_shift, end_pitch_shift]
+%
+% uses the voicebox toolbox
+
+
+% get gottal opening and closing instances
+[gci,goi] = dypsa(s,fs);
+
+
+S=s;
+
+
+end
